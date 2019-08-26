f0d1e8f58ed0e75e0d3dac6cb7d78baf3e7135be
Bruno Valle <brunolv6@gmail.com>
2019-08-26 16:15:52 -0300

    Centralize elements and got how to do the colors in diagonal

--- src/components/imagem/imagem.component.jsx
+++ src/components/imagem/imagem.component.jsx
@@ -3,26 +3,31 @@ import React from 'react';
 import './imagem.style.scss';
 
 import foto from '../../assets/eu1.jpg';
-import github from '../../assets/github.png';
+import github from '../../assets/github.svg';
 import linkedin from '../../assets/linkedin.svg';
 
 const Imagem = props => {
     return (
-        <div className="apresentacao">
-            <div className='circle'>
-                <div
-                className='imagem'
-                style={{
-                    backgroundImage: `url(${foto})`
-                }}      
-                />
+        <div>
+            <div className="apresentacao">
+                <div className='circle'>
+                    <div
+                    className='imagem'
+                    style={{
+                        backgroundImage: `url(${foto})`
+                    }}      
+                    />
+                </div>
+                <div className='name'><strong>Bruno Valle</strong></div>
+                <div className='profissao'>Desenvolvedor Front-End</div>
+                <div className='formas-contato'>
+                    <div className='logo-info'><img src={github} alt='github-acesso' className='logos'/></div>
+                    <div className='logo-info'><img src={linkedin} alt='github-acesso' className='logos'/></div>
+                </div>            
             </div>
-            <div><strong>Bruno Valle</strong></div>
-            <div>Desenvolvedor Front-End</div>
-            <div className='formas-contato'>
-                <div className='logos'><img src={github} alt='github-acesso'/></div>
-                <div className='logos'><img src={linkedin} alt='github-acesso'/></div>
-            </div>            
+            <div className='gradient'></div>
+            <div className='teste'>Teste</div>
+            <div className='gradient2'></div>
         </div>
     );
 };
