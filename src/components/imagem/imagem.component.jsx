4167a8f85454ceee3c744dc3583a81f4300711d5
Bruno Valle <brunolv6@gmail.com>
2019-08-27 08:36:19 -0300

    Add image of ecommerce project

--- src/components/imagem/imagem.component.jsx
+++ src/components/imagem/imagem.component.jsx
@@ -5,6 +5,7 @@ import './imagem.style.scss';
 import foto from '../../assets/eu1.jpg';
 import github from '../../assets/github.svg';
 import linkedin from '../../assets/linkedin.svg';
+import ecommerce from '../../assets/ecommerce.png';
 
 const Imagem = props => {
     return (
@@ -21,12 +22,14 @@ const Imagem = props => {
                 <div className='name'><strong>Bruno Valle</strong></div>
                 <div className='profissao'>Desenvolvedor Front-End</div>
                 <div className='formas-contato'>
-                    <div className='logo-info'><img src={github} alt='github-acesso' className='logos'/></div>
-                    <div className='logo-info'><img src={linkedin} alt='github-acesso' className='logos'/></div>
+                    <a href='https://github.com/brunolv6' rel="noopener noreferrer" target="_blank" className='logo-info'><img src={github} alt='github-acesso' className='logos'/></a>
+                    <a href="https://www.linkedin.com/in/bruno-leme-valle-6blv/" rel="noopener noreferrer" target="_blank" className='logo-info'><img src={linkedin} alt='github-acesso' className='logos'/></a>
                 </div>            
             </div>
             <div className='gradient'></div>
-            <div className='teste'>Teste</div>
+            <div className='teste'>
+                    <img className='ecommerce' src={ecommerce} alt='projeto1' />
+            </div>
             <div className='gradient2'></div>
         </div>
     );
