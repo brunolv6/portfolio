a42209139462d0e94638fcbe5ce3ed0008177b5a
Bruno Valle <brunolv6@gmail.com>
2019-08-28 14:58:16 -0300

    Get the logos in .svg and .png and style it

--- src/components/ecommerce/ecommerce.component.jsx
+++ src/components/ecommerce/ecommerce.component.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 import './ecommerce.style.scss'
 import ecommerceImage from '../../assets/ecommerce.png';
-/* import herokuLogo from '../../assets/heroku.svg'; */
+import herokuLogo from '../../assets/heroku.svg';
+import firebaseLogo from '../../assets/firebase.png';
+import nodeLogo from '../../assets/node.png';
+import stripeLogo from '../../assets/stripe.svg';
+import reduxLogo from '../../assets/redux.png';
+import reactLogo from '../../assets/react.png';
 
 import { Descricao } from '../descricao/descricao.component'
 
@@ -19,13 +24,49 @@ export const Ecommerce = () => (
                 alt='ecommerce'
             />
         </div>
-{/*         
+        <div className='ferramentas'>
         <div>
             <img 
-                className='imagem'
+                className='logo'
                 src={herokuLogo} 
                 alt='heroku'
             />
-        </div> */}
+        </div>
+        <div>
+            <img 
+                className='logo'
+                src={firebaseLogo} 
+                alt='firebase'
+            />
+        </div>
+        <div>
+            <img 
+                className='logo'
+                src={nodeLogo} 
+                alt='node'
+            />
+        </div>
+        <div>
+            <img 
+                className='logo'
+                src={reduxLogo} 
+                alt='redux'
+            />
+        </div>
+        <div>
+            <img 
+                className='logo'
+                src={reactLogo} 
+                alt='react'
+            />
+        </div>
+        <div>
+            <img 
+                className='logo'
+                src={stripeLogo} 
+                alt='stripe'
+            />
+        </div>
+        </div>
     </div>
 )
